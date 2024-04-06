b23639b558de111f9141821cc71c5fda5601ea9f
JoelsonTravassos <joelson.travassos@gmail.com>
2024-04-06 11:55:57 -0300

    Classe para tratar excecoes

---
+++ CONTROLE-FLUXO/src/TratamentoDeExcecoes/CepInvalidoException.java
@@ -0,0 +1,20 @@
+package TratamentoDeExcecoes;
+
+public class CepInvalidoException extends Exception {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("22555554");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("Cep invalido");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return cep;
+    }
+}
